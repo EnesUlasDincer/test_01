2e55400e5ea679ac073ccec76342205a99eba1b6
EnesUlasDincer <enes.ulas.dincer@gmail.com>
2024-12-18 17:49:34 +0100

    Cahnged the main.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ int main() {
 
     std::cout << "Hello" << std::endl;
 
-    std::cout << add(7,6) << std::endl;
+    std::cout << add(7,5) << std::endl;
 
+    std::cout << "Bye" << std::endl;
     std::cout << "Bye" << std::endl;
 
     writeMyName();    
